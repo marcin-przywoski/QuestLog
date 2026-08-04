ff7d0db3f335f12404efe9873a2a9f9c27914f8d
Marcin Przywóski <marcin.przywoski26@gmail.com>
2026-08-04 23:00:44 +0200

    Added Velopack for seamless auto-updates

--- QuestLog.GUI/Program.cs
+++ QuestLog.GUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using Velopack;
 
 namespace QuestLog.GUI;
 
@@ -9,9 +10,14 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // Velopack must run as early as possible in app startup.
+        // It may exit/restart the app at this point for update handling.
+        VelopackApp.Build().Run();
 
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
