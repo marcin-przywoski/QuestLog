eb2620e942eb91c81f6a53337736a641eaf83a42
Marcin Przywóski <marcin.przywoski26@gmail.com>
2026-08-06 19:18:02 +0200

    Added email service to constructor of MainWindow VM

--- QuestLog.GUI/ViewModels/MainWindowViewModel.cs
+++ QuestLog.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace QuestLog.GUI.ViewModels
+﻿using QuestLog.GUI.Interfaces;
+using QuestLog.GUI.Services;
+
+namespace QuestLog.GUI.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
+        private readonly IEmailService _emailService;
+
+
+        public MainWindowViewModel()
+        {
+            _emailService = new AppleScriptOutlookService();
+        }
+
+        public MainWindowViewModel(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
     }
 }
